4dca03a19681f5de74ede03303f322b4a4e465c7
Musavir Pasha <110371913+Musavircodes@users.noreply.github.com>
2025-02-01 13:18:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,21 @@ document.getElementById("contact-form").addEventListener("submit", function(even
         });
 });
 
-
+document.addEventListener("mousemove", (event) => {
+  document.querySelectorAll(".eye").forEach((eye) => {
+    let pupil = eye.querySelector(".pupil");
+    let eyeRect = eye.getBoundingClientRect();
+    let eyeCenterX = eyeRect.left + eyeRect.width / 2;
+    let eyeCenterY = eyeRect.top + eyeRect.height / 2;
+    let deltaX = event.clientX - eyeCenterX;
+    let deltaY = event.clientY - eyeCenterY;
+    let angle = Math.atan2(deltaY, deltaX);
+    let distance = Math.min(eyeRect.width / 4, Math.hypot(deltaX, deltaY) / 5);
+    pupil.style.transform = `translate(${Math.cos(angle) * distance}px, ${
+      Math.sin(angle) * distance
+    }px)`;
+  });
+});
 
 var canvas = document.getElementById('nokey'),
    can_w = parseInt(canvas.getAttribute('width')),
